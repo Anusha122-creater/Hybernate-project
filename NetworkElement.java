5d59f0ec692e091386348eca1f8d880fff766d56
Anusha Balasani <balasanianusha24@gmail.com>
2024-12-12 08:44:04 +0530

    Add files via upload

---
+++ NetworkElement.java
@@ -0,0 +1,48 @@
+
+package com.Telecommunication.entity;
+
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class NetworkElement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer networkElementId;
+
+    public Integer getNetworkElementId() {
+		return networkElementId;
+	}
+
+	public void setNetworkElementId(Integer networkElementId) {
+		this.networkElementId = networkElementId;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	private String type;
+    private String location;
+
+    @Override
+    public String toString() { return "NetworkElement{" + "networkElementId=" + networkElementId + '}'; }
+}
